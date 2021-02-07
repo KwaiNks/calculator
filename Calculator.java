31c1bf298cac9d00cafbab1b477602ac4ec55417
KwaiNks <richmondnkansah@gmail.com>
2021-02-06 23:57:49 -0500

    addition function implemented with test

--- Calculator.java
+++ Calculator.java
@@ -7,8 +7,6 @@ public class Calculator{
     public Calculator(){
         history = new History();
     }
-
-
 	public double additionOfTwoNumbers(double firstNumber, double secondNumber) {
         result = firstNumber + secondNumber;
         history.addToCalculatorHistory(firstNumber, '+', secondNumber, '=', result);
